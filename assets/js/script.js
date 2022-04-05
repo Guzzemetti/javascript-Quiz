c1e553e3d955ba0bb7f00fcddcbe5f6f2e1a9bf2
John Guzzetta <johnguzzettaiv@gmail.com>
2022-04-05 15:33:59 -0400

    Updated JS, was able to get my storage to pull an object and have it display on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,39 @@ var questionArticle = document.querySelector("#questions-article");
 var quizResults = document.querySelector("#quizResults");
 var timer = document.querySelector("#timer");
 // JS Variables
-
+var currentQuestionIndex = 0
 // timeRemaining is the variable used in my timer (line 40 as of 4/2) to identify the nunmber of seconds remaining
 
 
 // An array for my questions
-var questions = [
-    {   question: "What does 'www' stand for in a website browser?",
+var questionsArr = [
+    {
+        question: "What does 'www' stand for in a website browser?",
         choices: ["World Wide Web", "Western World Websites", "Where Websites Wumbo", "What was what"],
         answer: "World Wide Web"
     },
-    {   question: "How long is an Olympic swimming pool (in meters)?",
+    {
+        question: "How long is an Olympic swimming pool (in meters)?",
         choices: ["30", "50", "60", "75"],
         answer: "50"
     },
-    {   question: "What geometric shape is generally used for stop signs?",
+    {
+        question: "What geometric shape is generally used for stop signs?",
         choices: ["Hexagon", "Dodecahedron", "Octagon", "Trapezoid"],
         answer: "Octagon"
     },
-    {   question: "What is the name of the biggest technology company in South Korea?",
+    {
+        question: "What is the name of the biggest technology company in South Korea?",
         choices: ["Verizon", "Samsung", "Kia", "Hyundai"],
         answer: "Samsung"
     },
-    {   question: "Demolition of the Berlin wall separating East and West Germany began in what year?",
+    {
+        question: "Demolition of the Berlin wall separating East and West Germany began in what year?",
         choices: ["1988", "1987", "1986", "1989"],
         answer: "1989"
     },
-    {   question: "What is the most consumed manufactured drink in the world?",
+    {
+        question: "What is the most consumed manufactured drink in the world?",
         choices: ["Tea", "Coca-Cola", "Mike's Hard Lemonade", "Wine"],
         answer: "Tea"
     }
@@ -41,45 +47,55 @@ var questions = [
 function startQuiz() {
     // Sets the number of seconds for my timer
     var timeRemaining = 3;
-     setInterval(function(){
+    setInterval(function () {
         timeRemaining--;
         // If time remaining is equal to or greater than 0, the remaining time will be displayed
-        if (timeRemaining >= 0){
-        timer.textContent = timeRemaining + " seconds remaining.";
+        if (timeRemaining >= 0) {
+            timer.textContent = timeRemaining + " seconds remaining.";
         }
         // If the time remaining is equal to 0, then it displays a game over notice and clears the time remaining interval
-        if(timeRemaining === 0) {
+        if (timeRemaining === 0) {
             timer.textContent = "Game Over";
             clearInterval(timeRemaining);
             // Selects the Start Button from the HTML and then changes text when timer hits 0
             let startButton = document.querySelector("#startButton");
-                startButton.textContent = "Retry?"
+            startButton.textContent = "Retry?"
         }
     }, 1000);
 }
 
 
-
+localStorage.setItem("questions", JSON.stringify(questionsArr));
 
 // TODO - Make a function that grabs one question from the array to display at a time
 // I need this function to display a question and it's potential answers when the start button is clicked
 // This function should also accept input such as clicking on an answer or entering a choice number
 
-function displayQuestions() {
-        for (let i = 0; i < questions.length; i++) {
-        var questions = questions[i];
-        }
-        questionArticle.innerHTML = questions;
+function displayQuestions() { 
+    for (let index = 0; index < questionsArr.length; index++) {
+        let currentQuestionIndex = questionsArr[index];
+        document.querySelector("#questionsArticle").textContent = currentQuestionIndex;
+        currentQuestionIndex = JSON.parse(localStorage.getItem("questionsArr"));
 
+
+        
+    }
 };
+currentQuestionIndex = JSON.parse(localStorage.getItem("questionsArr"));
+console.log(questionsArr.question);
+console.log(questionsArr.choices);
+console.log(questionsArr.answer);
+// console.log(questionsArr);
+// console.log(questionsArr);
 
 // TODO - Need a function to subtract time from timeRemaining for incorrect answers
 // TODO - Need a GameOver display when total time runs out
-    // As of now, I have a Game-Over message that displays in the Timer Box once it reaches 0
+// As of now, I have a Game-Over message that displays in the Timer Box once it reaches 0
 // TODO - Need a Congrats display for when all questions have been answered correctly
 // TODO - use localStorage to store/get wins/loses and track right questions "3/5 correct"
 // TODO - Make a reset button for game stats
 // TODO - Allow users to input their intials for a high score chart
 
 // Event listener for my start button which initiates my timer
-startButton.addEventListener("click", startQuiz, displayQuestions);
+startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", displayQuestions);
